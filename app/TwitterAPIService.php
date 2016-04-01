8162972806fa5ae2650c48bd02e5967721f67ec3
Rob LaCagnina <robert.lacagnina@harpercollins.com>
2016-04-01 20:22:46 +0000

    twitter auth

--- app/TwitterAPIService.php
+++ app/TwitterAPIService.php
@@ -13,26 +13,39 @@ class TwitterAPIService implements IApiService {
 	const API_AUTH_URL = 'https://api.twitter.com/oauth2/token';
 	const API_URL = 'https://api.twitter.com/1.1/search/tweets.json';
 
-	private $guzzleHttp;
+	private $guzzleHttpClient;
 
 	public function __construct(Client $client) {
-		$this->guzzleHttp = $client;
+		$this->guzzleHttpClient = $client;
 	}
 
 	public function Search( $searchQuery ) {
 
 
-
+		//return an array of tweet objects
 		return array();
 	}
 
 	private function CallAPI() {
+
+		$apiKey = env("TWITTER_API_KEY");
+		$apiSecret = env("TWITTER_API_SECRET");
+
+		$encodedKey = urlencode($apiKey);
+
+		$bearerTokenCredentials = $encodedKey . ":" . $apiSecret;
+
+		$base64Credentials = base64_encode($bearerTokenCredentials);
+
+
 		//authenticate with Twitter API
-		$this->guzzleHttp->get();
+		$this->guzzleHttpClient->post(
+			self::API_AUTH_URL,
+			array("Authorization" => $base64Credentials),
+			"grant_type=client_credentials"
+			)->send();
 
-
-
-		$apiResponse = $this->guzzleHttp->get(self::API_URL . "arguments go here...")->send();
+		$apiResponse = $this->guzzleHttpClient->get(self::API_URL . "arguments go here...")->send();
 
 		if($apiResponse->isSucessful) {
 			$responseArray = $apiResponse->json();
