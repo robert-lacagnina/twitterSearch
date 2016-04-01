eba4ff761b0d84668cce7d04168a368a56d7baa2
Rob LaCagnina <robert.lacagnina@harpercollins.com>
2016-04-01 20:08:38 +0000

    updated api service to authenticate, adding env

--- app/TwitterAPIService.php
+++ app/TwitterAPIService.php
@@ -1,18 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: RobertLaCagnina
- * Date: 4/1/2016
- * Time: 2:11 PM
- */
 
 namespace App;
 
+use GuzzleHttp\Client;
+use Illuminate\Http\Exception\HttpResponseException;
 
-class TwitterAPIService implements IApiService{
+/*@Class
+This class takes in a string search query and returns an array of tweet objects.
+*/
+class TwitterAPIService implements IApiService {
+
+	const API_AUTH_URL = 'https://api.twitter.com/oauth2/token';
+	const API_URL = 'https://api.twitter.com/1.1/search/tweets.json';
+
+	private $guzzleHttp;
+
+	public function __construct(Client $client) {
+		$this->guzzleHttp = $client;
+	}
 
 	public function Search( $searchQuery ) {
-		// TODO: Implement Search() method.
+
+
+
+		return array();
+	}
+
+	private function CallAPI() {
+		//authenticate with Twitter API
+		$this->guzzleHttp->get();
+
+
+
+		$apiResponse = $this->guzzleHttp->get(self::API_URL . "arguments go here...")->send();
+
+		if($apiResponse->isSucessful) {
+			$responseArray = $apiResponse->json();
+
+			foreach($responseArray as $tweet) {
+				//parse api response array and create object
+			}
+		}
+		else {
+			throw new HttpResponseException( $apiResponse );
+		}
 	}
 
 }
