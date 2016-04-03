5b56c61e98f10c19cf7806b1ea65d13bc8568c80
Rob LaCagnina <robert.lacagnina@gmail.com>
2016-04-03 20:37:59 +0000

    formatting date

--- public/js.js
+++ public/js.js
@@ -7,17 +7,18 @@ $(document).ready(function () {
         console.log(searchQuery);
 
         $.get('/search?q=' + encodeURIComponent(searchQuery), function (data) {
-
             //remove all result rows from the table
             $('tr.result-row').remove();
 
             //clear searchbox
             $twitterSearch.val('');
-
             data.tweets.forEach(function (tweet) {
+
+                var formattedDate = new Date(tweet.createdTimeStamp * 1000);
+
                 $('.search-results-tbody').append(
                     '<tr class="result-row"><td>' + tweet.handle +  '</td>' +
-                    '<td>' + tweet.createTimeStamp + '</td>'
+                    '<td>' + formattedDate + '</td>'
                     + '<td>' + tweet.tweetText + '</td></tr>');
             });
         });
